ed4a658f4f1303ec6cc0f5f50d4104bbe5213834
campbelg <campbelg@oclc.org>
2014-12-03 11:51:11 -0500

    Added project files.

---
+++ src/main/java/com/tallgeorge/docker1/Main.java
@@ -0,0 +1,10 @@
+package com.tallgeorge.docker1;
+
+/**
+ * Created by campbelg on 12/3/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Docker");
+    }
+}
